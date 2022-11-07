6c791e626655135412378299c11d3cf1ca98fff9
Magnogen <hydronesium@gmail.com>
2022-11-07 11:45:41 +0000

    remove, rearrange and SPEED UP

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@
 
   let [width, height] = [c.width, c.height] = [256, 256];
   
-  let grid = [...Array(height)].map((_, x) => [...Array(width)].map((_, y) => {
+  let grid = [...Array(width)].map((_, x) => [...Array(height)].map((_, y) => {
     // return 0;
     return 0|(Math.random()*3);
   } ));
@@ -28,26 +28,27 @@
     'r': (x, y) => Math.random() < 0.5,
     'd': (x,y,r)=> Math.abs(x) + Math.abs(y) <= r,
   };
+  // col: abgr
   let cells = [
     {
       id: 'a',
-      col: 'hsl(210, 60%, 50%)',
+      col: 0xffcc7f33, // 'hsl(210, 60%, 50%)',
       // zone_type: 'd',
-      r: 5,
+      radius: 5,
       convert: ({ a, b, c }) => ({ b: a })
     },
     {
       id: 'b',
-      col: 'hsl(220, 60%, 50%)',
+      col: 0xffcc6633, // 'hsl(220, 60%, 50%)',
       // zone_type: 'd',
-      r: 5,
+      radius: 5,
       convert: ({ a, b, c }) => ({ c: b })
     },
     {
       id: 'c',
-      col: 'hsl(230, 60%, 50%)',
+      col: 0xffcc4d33, // 'hsl(230, 60%, 50%)',
       // zone_type: 'd',
-      r: 5,
+      radius: 5,
       convert: ({ a, b, c }) => ({ a: c })
     }
   ];
@@ -60,40 +61,35 @@
   function update(x, y) {
     let me = grid[x][y];
     let mapper = { ...map, ...cells[me].convert(map)};
-    const r = cells[me].r;
-    for (let _X = max(0,x-r); _X <= min(x+r, width-1 ); _X++)
-    for (let _Y = max(0,y-r); _Y <= min(y+r, height-1); _Y++) {
-    // for (let _X = x-cells[me].r; _X <= x+cells[me].r; _X++)
-    // for (let _Y = y-cells[me].r; _Y <= y+cells[me].r; _Y++) {
-      let X = _X, Y = _Y;
-      // if (X > width-1) X -= width;
-      // if (X < 0) X += width;
-      // if (Y > height-1) Y -= height;
-      // if (Y < 0) Y += height;
-      
+    const r = cells[me].radius;
+    const left = max(0, x-r),
+          top = max(0, y-r);
+    let idata = ctx.getImageData(left, top, 1+2*r, 1+2*r);
+    let pixels = new Uint32Array(idata.data.buffer);
+    
+    for (let X = left; X <= min(x+r, width-1 ); X++)
+    for (let Y = top; Y <= min(y+r, height-1); Y++) {
       let cell_i = grid[X][Y];
       let cell = cells[cell_i];
-      if (!zone_types[cells[me].zone_type](_X-x, _Y-y, r)) continue;
+      if (!zone_types[cells[me].zone_type](X-x, Y-y, r)) continue;
       let mapped = mapper[cell.id];
       if (Array.isArray(mapped)) mapped = choose(mapped);
       cell_i = grid[X][Y] = mapped;
-      ctx.fillStyle = cells[cell_i].col;
-      ctx.fillRect(X, Y, 1, 1);
+      
+      const i = (X-left) + (Y-top)*(1+2*r);
+      pixels[i] = cells[cell_i].col;
     }
+    ctx.putImageData(idata, left, top)
   }
   
-  for (let x = 0; x < grid.length; x++) for (let y = 0; y < grid[x].length; y++) {
-    ctx.fillStyle = cells[grid[x][y]].col;
-    ctx.fillRect(x, y, 1, 1);
-  }
-  
-  while (true) {
+  do {
     let last = performance.now();
-    while (performance.now() - last < 1000/30) {
-      update(0|(Math.random()*width), 0|(Math.random()*width));
+    let count = 0;
+    while (performance.now() - last < 1000/30 && count++ < 2*width) {
+      update(0|(Math.random()*width), 0|(Math.random()*height));
     }
     await new Promise(requestAnimationFrame);
-  }
+  } while (true);
 
 })();
 
